f61575a436d97f6b4b38a5fa8a1356df0fb4f041
Wood <jhuangchenleo@gmail.com>
2025-06-04 20:32:00 +0800

    refactor: make ToolPanel singleton、composition

--- src/GUI/ToolPanel.java
+++ src/GUI/ToolPanel.java
@@ -8,59 +8,77 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+singleton、composition
+ */
+
 public class ToolPanel {
-    private final Canvas canvas = Canvas.getInstance();
+    private static Canvas canvas;
+    private static JPanel toolsPanel;
+    private static List<JButton> buttons;
 
-    private final List<JButton> buttons = new ArrayList<>();
-    private final JPanel toolsPanel = new JPanel();
+    private static Mode selectMode;
+    private static Mode rectMode;
+    private static Mode ovalMode;
+    private static Mode assLinkMode;
+    private static Mode depdLinkMode;
+    private static Mode genLinkMode;
+    private static Mode compLinkMode;
 
-    private final Mode selectMode = new SelectMode();
-    private final Mode rectMode = new RectMode();
-    private final Mode ovalMode = new OvalMode();
-    private final Mode assLinkMode = new AssLinkMode();
-    private final Mode depdLinkMode = new DepdLinkMode();
-    private final Mode genLinkMode = new GenLinkMode();
-    private final Mode compLinkMode = new CompLinkMode();
+    public static JPanel getPanel() {
+        if (toolsPanel == null) {
+            createPanel();
+        }
+        return toolsPanel;
+    }
 
-    public JPanel getToolsPanel(){ return this.toolsPanel; }
+    private static void createPanel() {
+        canvas = Canvas.getInstance();
+        buttons = new ArrayList<>();
+        toolsPanel = new JPanel();
+        toolsPanel.setLayout(new GridLayout(7,1));
+        toolsPanel.setBackground(Color.gray);
 
-    public ToolPanel() {
-        this.toolsPanel.setLayout(new GridLayout(7,1));
-        this.toolsPanel.setBackground(Color.gray);
+        selectMode = new SelectMode();
+        rectMode = new RectMode();
+        ovalMode = new OvalMode();
+        assLinkMode = new AssLinkMode();
+        depdLinkMode = new DepdLinkMode();
+        genLinkMode = new GenLinkMode();
+        compLinkMode = new CompLinkMode();
 
         JButton bTmp;
 
         bTmp = getButton("Sel", "res/icon_select.png");
         bTmp.addActionListener(e->canvas.setMode(selectMode));
-        this.toolsPanel.add(bTmp);
+        toolsPanel.add(bTmp);
 
         bTmp = getButton("Ass","res/icon_association.png");
         bTmp.addActionListener(e->canvas.setMode(assLinkMode));
-        this.toolsPanel.add(bTmp);
+        toolsPanel.add(bTmp);
 
         bTmp = getButton("Depd","res/icon_dependency.png");
         bTmp.addActionListener(e->canvas.setMode(depdLinkMode));
-        this.toolsPanel.add(bTmp);
+        toolsPanel.add(bTmp);
 
         bTmp = getButton("Gen", "res/icon_generalization.png");
         bTmp.addActionListener(e->canvas.setMode(genLinkMode));
-        this.toolsPanel.add(bTmp);
+        toolsPanel.add(bTmp);
 
         bTmp = getButton("Com", "res/icon_composition.png");
         bTmp.addActionListener(e->canvas.setMode(compLinkMode));
-        this.toolsPanel.add(bTmp);
+        toolsPanel.add(bTmp);
 
         bTmp = getButton("Rec", "res/icon_rect.png");
         bTmp.addActionListener(e->canvas.setMode(rectMode));
-        this.toolsPanel.add(bTmp);
+        toolsPanel.add(bTmp);
 
         bTmp = getButton("Ova", "res/icon_oval.png");
         bTmp.addActionListener(e->canvas.setMode(ovalMode));
-        this.toolsPanel.add(bTmp);
-
+        toolsPanel.add(bTmp);
     }
 
-    private void buttonClicked(JButton clickedBtn) {
+    private static void buttonClicked(JButton clickedBtn) {
         for (JButton btn : buttons) {
             if (btn == clickedBtn) {
                 btn.setBackground(Color.GRAY);
@@ -73,9 +91,9 @@ public class ToolPanel {
         toolsPanel.repaint();
     }
 
-    private final ActionListener actionListener = e -> buttonClicked((JButton) e.getSource());
+    private static final ActionListener actionListener = e -> buttonClicked((JButton) e.getSource());
 
-    private JButton getButton(String text, String imgURL) {
+    private static JButton getButton(String text, String imgURL) {
         JButton button = new JButton(text);
         button.setBackground(Color.WHITE);
         button.addActionListener(actionListener);
@@ -93,7 +111,7 @@ public class ToolPanel {
             button.setIcon(icon);
         }
 
-        this.buttons.add(button);
+        buttons.add(button);
         return button;
     }
 }
